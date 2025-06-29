28ba47203f4cda023c238d3e87ea7da9e7cea6ff
sujazoya <sujazoya@gmail.com>
2025-06-29 16:03:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,6 +87,7 @@ def parse_embroidery():
 
 if __name__ == '__main__':
     import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
