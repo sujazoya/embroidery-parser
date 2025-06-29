dad4f1c98315660d2e41191ebb2eb182cfd2c4e5
sujazoya <sujazoya@gmail.com>
2025-06-29 15:59:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -86,4 +86,7 @@ def parse_embroidery():
         return jsonify({"success": False, "error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
+
