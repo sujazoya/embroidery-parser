5c0fb6f9826578576332ca55aaaa51f58ced6855
sujazoya <sujazoya@gmail.com>
2026-05-06 01:00:40 +0530

    1

--- app.py
+++ app.py
@@ -90,4 +90,11 @@ if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Embroidery Parser API is running 🚀"
 
